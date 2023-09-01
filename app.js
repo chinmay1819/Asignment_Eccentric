16f892af1a28950464834d321605133ad574f523
Chinmay <chinmaydeshpande34@gmail.com>
2023-09-01 18:25:07 +0530

    feat
    added room based chatting feature

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-const express=require('express');
-const app=express();
-const http=require('http').createServer(app)
-const io=require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-http.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`)
-})
-app.use(express.static(__dirname+'/public'))
-app.get('/',(req,res)=>{
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-})
+});
 
+// Store a mapping of socket IDs to room names
+const socketToRoom = {};
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('Connected...');
-    socket.on('message',(msg)=>{
-        socket.broadcast.emit('message',msg)
-    })
-})
+    socket.on('join', (room) => {
+ 
+        const previousRoom = socketToRoom[socket.id];
+        if (previousRoom) {
+            socket.leave(previousRoom);
+        }
 
+        socket.join(room);
+        socketToRoom[socket.id] = room;
+
+    });
+
+    // Handle sending messages in a specific room
+    socket.on('message', (msg) => {
+        const room = socketToRoom[socket.id];
+        if (room) {
+            // Emit the message to all clients in the same room
+            io.to(room).emit('message', msg);
+        }
+    });
+});
--- public/client.js
+++ public/client.js
@@ -1,54 +1,55 @@
-const socket=io()
+const socket = io();
 let userName;
 let roomId;
-let textarea=document.querySelector('#textarea')
-let messageArea=document.querySelector('.message__area')
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message__area');
 
-do{
-    userName=prompt('Please Enter your Name : ')
-}while(!userName);
+do {
+    userName = prompt('Please Enter your Name: ');
+} while (!userName);
 
-do{
-    roomId=prompt('Please Enter room id :')
-}while(!roomId);
-textarea.addEventListener('keyup',(e)=>{
-    if(e.key==='Enter'){
-        sendMessage(e.target.value)
+do {
+    roomId = prompt('Please Enter room id: ');
+} while (!roomId);
+
+// Send a 'join' event to the server to join the specified room
+socket.emit('join', roomId);
+
+textarea.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        sendMessage(e.target.value);
     }
-})
+});
 
-function sendMessage(message){
-    let msg={
-        user:userName,
-        message:message.trim()
-
-    }
-    //Appending the message
-    appendMessage(msg,'outgoing');
-    textarea.value=''
-    scrollToBottom()
-    //send to server 
-    socket.emit('message',msg)
-
-
+function sendMessage(message) {
+    let msg = {
+        user: userName,
+        message: message.trim(),
+    };
+    // Appending the message
+    appendMessage(msg, 'outgoing');
+    textarea.value = '';
+    scrollToBottom();
+    // Send the message to the server with the room ID
+    socket.emit('message', msg);
 }
 
-function appendMessage(msg,type){
-    let mainDiv=document.createElement('div')
-    let className= type
-    mainDiv.classList.add(className,'message')
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div');
+    let className = type;
+    mainDiv.classList.add(className, 'message');
 
-    let markup=`
+    let markup = `
         <h4>${msg.user}</h4>
         <p>${msg.message}</p>
-    `    
-    mainDiv.innerHTML=markup
-    messageArea.appendChild(mainDiv)
+    `;
+    mainDiv.innerHTML = markup;
+    messageArea.appendChild(mainDiv);
 }
 
 if ('serviceWorker' in navigator && 'Notification' in window) {
-    // Register service worker
-    navigator.serviceWorker.register('/service-worker.js')
+      // Register service worker
+      navigator.serviceWorker.register('/service-worker.js')
       .then(registration => {
         console.log('Service Worker registered:', registration);
       })
@@ -64,31 +65,32 @@ if ('serviceWorker' in navigator && 'Notification' in window) {
         }
       });
     }
-  
-    // After receiving a new message
-    socket.on('message', msg => {
+
+socket.on('message', msg => {
+  // Check if the message is not from yourself
+  if (msg.user !== userName) {
       appendMessage(msg, 'incoming');
       scrollToBottom();
   
       // Check if the page is not in focus
       if (!document.hasFocus()) {
-        // Display a notification
-        if (Notification.permission === 'granted') {
-          const notification = new Notification('New Message', {
-            body: `${msg.user}: ${msg.message}`,
-            icon: '/chat2.png' // Path to the notification icon
-          });
+          // Display a notification
+          if (Notification.permission === 'granted') {
+              const notification = new Notification('New Message', {
+                  body: `${msg.user}: ${msg.message}`,
+                  icon: '/chat2.png' // Path to the notification icon
+              });
   
-          // Handle notification click event if needed
-          notification.onclick = () => {
-            // Do something when the user clicks the notification
-          };
-        }
+              // Handle notification click event if needed
+              notification.onclick = () => {
+                  // Do something when the user clicks the notification
+              };
+          }
       }
-    });
+  }
+});
 
     function scrollToBottom(){
        messageArea.scrollTop=messageArea.scrollHeight
     }
 }
-  
